2aa3613f1cfc6d63fc83414eaa389e91b69c808d
Steve Armstrong <steve@horsefire.com>
2012-03-31 16:16:34 -0700

    Validate the type tag, and the time properties for events

---
+++ validate_doc_update.js
@@ -0,0 +1,24 @@
+function(newDoc, oldDoc, userCtx) {
+	var fail = function(message) { throw({"forbidden":message}); };
+	
+	if (!("type" in newDoc)) { fail("Documents must have a type"); }
+	switch (newDoc.type) {
+		case "event":
+			if ( !("when" in newDoc) || !("utc" in newDoc.when)
+				|| !("local" in newDoc.when) || !("offset" in newDoc.when) ) {
+				fail("Events must have utc, local and offset fields in when");
+			}
+			if (!Array.isArray(newDoc.when.local) || newDoc.when.local.length != 5) {
+				fail("when.local must be a 5 element array");
+			}
+			if (typeof(newDoc.when.utc) != "number") {
+				fail("when.utc must be an integer");
+			}
+			if (typeof(newDoc.when.offset) != "number") {
+				fail("when.offset must be an integer");
+			}
+			break;
+		default:
+			fail("Unsupported document type: "+newDoc.type);
+	}
+}
